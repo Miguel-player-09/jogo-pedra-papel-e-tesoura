1144892d335df25fd54a5340a3868ecf4ce2f0ad
Karina <kbrilhante@gmail.com>
2022-11-27 20:16:34 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+objValores = {
+    "pedra": '<i class="fa-regular fa-hand-back-fist"></i>',
+    "papel": '<i class="fa-regular fa-hand"></i>',
+    "tesoura": '<i class="fa-regular fa-hand-scissors"></i>'
+}
+
+function jogar(valor) {
+    console.log(valor);
+}
